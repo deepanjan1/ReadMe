b80077b3eaaf717e6bfa26cfd1a433c61c672e5c
Deepanjan De <deep@deepcreations.net>
2021-01-10 14:42:59 -0500

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Deepanjan De on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
